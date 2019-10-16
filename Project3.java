6d04c09f5a39a03dbaa5b9af8f7a370ea114cce1
Gavavon <44536632+Gavavon@users.noreply.github.com>
2019-10-16 19:09:45 -0400

    Added Files
    I added a project 3 file which is an eclipse fill that has list interface already on it and I added the nodes file that needs to be updated for doubly linked lists.

---
+++ Project3.java
@@ -0,0 +1,55 @@
+package adts;
+
+import interfaces.ListInterface;
+
+public class Project3<E> implements ListInterface<E> {
+
+	@Override
+	public void add(E element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean remove(E element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(E element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E get(E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void resetIterator() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public E getNextItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
